9676418742dd4bf08af678ffb6b0fc5dc35eda46
Abdirahman005 <abdirahman.abdullah@student.moringaschool.com>
2025-06-30 19:52:10 +0300

    added updated file

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,33 +2,15 @@ const projects = [
   {
     title: "Task manager application",
     image: "/task-manager.png",
-    link: "https://task-manager-indol-six.vercel.app/", // Add your actual link
+    link: "https://task-manager-indol-six.vercel.app/", 
   },
   {
-    title: "Medicare HMS",
-    image: "/images/medicare-hms.jpg",
-    link: "https://your-link.com/medicare-hms",
+    title: "School registration system",
+    image: "/school-registration.png",
+    link: "https://school-learning-and-registration-sy.vercel.app/",
   },
-  {
-    title: "Connect–Matic",
-    image: "/images/connect-matic.jpg",
-    link: "https://your-link.com/connect-matic",
-  },
-  {
-    title: "Udgoon Hub",
-    image: "/images/udgoon-hub.jpg",
-    link: "https://your-link.com/udgoon-hub",
-  },
-  {
-    title: "Biogex Pharma LTD",
-    image: "/images/biogex.jpg",
-    link: "https://your-link.com/biogex-pharma",
-  },
-  {
-    title: "Hospital Management System",
-    image: "/images/hospital-system.jpg",
-    link: "https://your-link.com/hospital-system",
-  },
+ 
+  
 ];
 
 export default projects;
